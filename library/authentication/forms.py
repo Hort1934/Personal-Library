ab478d71938061b668ad68ad3d497e5d9eb17a05
Ihor Marchenko <igormarchenko1934@gmail.com>
2023-11-09 12:10:55 +0200

    add background and some changes with form registr

--- library/authentication/forms.py
+++ library/authentication/forms.py
@@ -6,7 +6,7 @@ class RegistrationForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     confirm = forms.CharField(widget=forms.PasswordInput)
     first_name = forms.CharField()
-    middle_name = forms.CharField(required=False)
+    # middle_name = forms.CharField(required=False)
     last_name = forms.CharField()
     role = forms.ChoiceField(choices=[(0, 'Visitor'), (1, 'Librarian')])
 
@@ -14,7 +14,3 @@ class RegistrationForm(forms.Form):
 class LoginForm(forms.Form):
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput)
-
-
-
-
