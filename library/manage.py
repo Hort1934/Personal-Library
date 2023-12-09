1f1f250586ac1b5cc20558fe5a572670cc3a4df6
Користувач <andrey2005bon@gmail.com>
2023-12-09 18:21:55 +0200

    add all upd files

--- library/manage.py
+++ library/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+# test
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library.settings')
