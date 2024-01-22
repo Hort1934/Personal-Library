3a01c5eca515381ae399751e1f7f87aa2f2b76d8
Ihor Marchenko <igormarchenko1934@gmail.com>
2024-01-22 16:22:09 +0200

    remove block readers

--- library/readers/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Reader
-
-admin.site.register(Reader)
--- library/readers/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import messages
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-
-from .models import Reader
-
-def reader_list(request):
-    readers = Reader.objects.all()
-    return render(request, 'reader_list.html', {'readers': readers})
