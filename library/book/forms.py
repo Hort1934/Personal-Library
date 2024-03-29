5843ea9990230a9ed38b8293644a61abebdfe2d7
Ihor Marchenko <igormarchenko1934@gmail.com>
2024-03-29 16:50:54 +0200

    add isbn and remove count from book

--- library/book/forms.py
+++ library/book/forms.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator
-from .models import Book
 from django import forms
+from .models import Book
+from author.models import Author
 
 
 class BookFilterForm(forms.Form):
@@ -12,10 +13,11 @@ class BookFilterForm(forms.Form):
 class BookForm(forms.ModelForm):
     class Meta:
         model = Book
-        fields = ['name', 'description', 'count', 'date_of_issue', 'image']
+        fields = ['name', 'description', 'isbn', 'date_of_issue', 'image', 'author']
 
     # Додайте поле для зображення
     image = forms.ImageField(required=False, label='Image')
+    author = forms.ModelChoiceField(queryset=Author.objects.all(), required=False, label='Author', empty_label=None)
 
     # Якщо ви хочете додати валідатор для поля count, ви можете зробити це так
     count = forms.IntegerField(validators=[MinValueValidator(0)])
