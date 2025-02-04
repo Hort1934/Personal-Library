bc5aa28de9f6630c95f085f2b3f9cebb0d4f1711
Користувач <andrey2005bon@gmail.com>
2025-02-04 17:04:02 +0200

    add favicon.ico and clear code

--- library/library/settings.py
+++ library/library/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = '_y2b#-m(nwf8irkpgs)wpg+-e$#_7^xaevp^me4+u4ov+3fyw*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
